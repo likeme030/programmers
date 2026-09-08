4f35a6e8a19644922373c8d7cc6244febe2beaf0
Daisy <142479520+likeme030@users.noreply.github.com>
2026-09-08 09:01:40 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String my_string, int k) {
-         
-        String a = "";
+        StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i<k; i++){
-            a += my_string;
+        for (int i = 0; i < k; i++) {
+            sb.append(my_string);
         }
-        return a;
+        
+        return sb.toString();
     }
 }

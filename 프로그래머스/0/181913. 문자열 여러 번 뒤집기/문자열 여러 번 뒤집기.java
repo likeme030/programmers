cd41c3449fb7c7eed71892a5f5121fa00968c332
Daisy <142479520+likeme030@users.noreply.github.com>
2026-09-16 22:10:44 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.55 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,23 +1,20 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
         
-        char[] arr = my_string.toCharArray();
-
+        char[] arr = my_string.toCharArray(); // String을 통째로 char 배열로 바꿀 수 있는 함수라네요 외워야지 .. 
         
-      for(int[] query : queries) {
-          int s = query[0];
-          int e = query[1];
-          
-          while(s<e){
-              char temp = arr[s];
-              arr[s] = arr[e];
-              arr[e] = temp;
-              
-              s++;
-              e--;
-          }
-      }
-        
-        return new String(arr);
+        for(int i = 0; i < queries.length; i++){
+                int left = queries[i][0];
+                int right = queries[i][1];
+            
+            while(left < right){
+                char t = arr[left];
+                arr[left] = arr[right];
+                arr[right] = t;
+                left++;
+                right--;
+            }
+        }  
+        return new String(arr); // char[] → String 하는 방법이래요
     }
 }
